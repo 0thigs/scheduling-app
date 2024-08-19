3bb36d782aeb43861ce081ec8c75c1fc0f2fe7de
0thigs <machinimavlogs@gmail.com>
2024-08-18 22:26:31 -0300

    🖥scripts: remove logs from middlewares

--- src/middleware.js
+++ src/middleware.js
@@ -4,9 +4,7 @@ export function middleware(request) {
   const { pathname } = new URL(request.url);
   if (pathname === '/') {
     const url = request.nextUrl.clone()
-    console.log(url)
     url.pathname = '/login'
-    console.log(url)
     return NextResponse.redirect(url)
   }
   return NextResponse.next();
