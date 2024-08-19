05449e9d8537933eb3d3af93202c668c7338bd12
0thigs <machinimavlogs@gmail.com>
2024-08-19 10:45:01 -0300

    💻script: rename middleware to typescript

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function middleware(request:any) {
   const { pathname } = new URL(request.url);
   if (pathname === '/') {
     const url = request.nextUrl.clone()
