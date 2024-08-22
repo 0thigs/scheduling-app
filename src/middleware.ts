8158c49b06836b6d4378fb26f58566034be1fccc
0thigs <machinimavlogs@gmail.com>
2024-08-21 22:56:38 -0300

    🖥scripts: add error treat in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,15 @@
 import { NextResponse } from "next/server";
+import supabase from "./config/supabaseClient";
 
 export function middleware(request:any) {
   const { pathname } = new URL(request.url);
+
   if (pathname === '/') {
     const url = request.nextUrl.clone()
     url.pathname = '/login'
+    console.log(supabase)
     return NextResponse.redirect(url)
   }
+
   return NextResponse.next();
 }
