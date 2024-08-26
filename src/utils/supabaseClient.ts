162c931f4c16429c2ae0d8f75beba153b8dd91ab
0thigs <machinimavlogs@gmail.com>
2024-08-25 23:09:12 -0300

    ♻refactor: update code

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,12 +1,12 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://smrgbhcfcgryecbmlvqe.supabase.co';
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_URL = 'https://smrgbhcfcgryecbmlvqe.supabase.co'
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 if (!SUPABASE_KEY) {
-    throw new Error('A chave SUPABASE_KEY não está definida no ambiente.');
+    throw new Error('A chave SUPABASE_KEY não está definida no ambiente.')
 }
 
-const supabase: SupabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase: SupabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-export default supabase;
+export default supabase
