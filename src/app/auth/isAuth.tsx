a0664820ed775b43037a19190380e8b3bafada0f
0thigs <machinimavlogs@gmail.com>
2024-08-31 20:47:45 -0300

    🖥script: create is auth method

---
+++ src/app/auth/isAuth.tsx
@@ -0,0 +1,12 @@
+import supabase from "../../utils/supabaseClient";
+
+export default async function isAuth() {
+    const isAuth = await supabase.auth.getSession()
+    if (isAuth.data.session?.access_token) {
+        console.log('Logged in', isAuth.data.session)
+        return true
+    } else {
+        console.log('Not logged in')
+        return false
+    }
+}
