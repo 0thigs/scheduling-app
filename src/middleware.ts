2f50a8da8e55e435b03b5fdcfb6d76a377c98d18
0thigs <machinimavlogs@gmail.com>
2024-08-31 20:46:44 -0300

    🖥scripts: updating middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,25 @@
 import { NextResponse } from "next/server";
+import supabase from "./utils/supabaseClient";
 
-export function middleware(request:any) {
+export async function middleware(request: any) {
   const { pathname } = new URL(request.url);
 
-  if (pathname === '/') {
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
+  if (pathname.startsWith('/_next') || pathname.startsWith('/static') || pathname.startsWith('/favicon.ico') || pathname.startsWith('/register') || pathname.startsWith('/about')) {
+    return NextResponse.next();
+  }
+
+  const { data: { session } } = await supabase.auth.getSession();
+
+  if (session) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/dashboard';
+    return NextResponse.redirect(url);
+  }
+  
+  if (!session && pathname !== '/login') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/login';
+    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
