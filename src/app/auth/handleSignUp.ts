42aacdc26ac34dceba86cfef07d10553b859e608
0thigs <machinimavlogs@gmail.com>
2024-09-02 15:42:21 -0300

    🖥scripts: create userServices that uses addUser

---
+++ src/app/auth/handleSignUp.ts
@@ -0,0 +1,10 @@
+import { addUser } from "../services/userService";
+
+export async function handleSignUp(email: string, name: string) {
+    try {
+      const userData = await addUser(email, name);
+      console.log('Usuário registrado com sucesso:', userData);
+    } catch (error: any) {
+      console.error('Erro ao registrar usuário:', error.message);
+    }
+  }
