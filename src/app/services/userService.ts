0e073892ff180ef8cdeddba6b9f970d3b2d33c48
0thigs <machinimavlogs@gmail.com>
2024-09-02 15:51:58 -0300

    🖥scripts: create add user

---
+++ src/app/services/userService.ts
@@ -0,0 +1,19 @@
+import supabase from "../../utils/supabaseClient";
+
+export async function addUser(email: string, name: string) {
+    const { data, error } = await supabase
+        .from("users")
+        .insert([
+            {
+                email: email,
+                name: name,
+            },
+        ])
+        .single();
+
+    if (error) {
+        console.log("Error adding user:", error);
+        return false;
+    }
+    return true;
+}
