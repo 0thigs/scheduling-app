a45b0145ed953f46ed14f5ba7739def7fcdb3224
0thigs <machinimavlogs@gmail.com>
2024-09-04 12:25:02 -0300

    💻scripts: update handle signup

--- src/app/auth/handleSignUp.ts
+++ src/app/auth/handleSignUp.ts
@@ -4,7 +4,9 @@ export async function handleSignUp(email: string, name: string) {
     try {
       const userData = await addUser(email, name);
       console.log('Usuário registrado com sucesso:', userData);
+      return true
     } catch (error: any) {
       console.error('Erro ao registrar usuário:', error.message);
+      return false
     }
   }
