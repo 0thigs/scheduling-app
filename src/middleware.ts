1eadc81432deba721aee92a7f2b412f5a97eb107
0thigs <machinimavlogs@gmail.com>
2024-09-08 15:32:16 -0300

    🖥script: update middleware to use the class

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,22 @@
 import { NextResponse } from "next/server";
 import supabase from "./utils/supabaseClient";
-import isAuth from "./app/auth/isAuth";
+import AuthUser from "./app/auth/authUser";
 
 export async function middleware(request: any) {
+
+  const authUser = new AuthUser();
   const { pathname } = new URL(request.url);
 
-  const authenticated = await isAuth();
-  console.log('Authenticated:', authenticated);
-  if (authenticated) {
+  const isLoggedIn = await authUser.isAuth();
+  console.log("Autenticado no middleware", isLoggedIn);
+
+  if (isLoggedIn) {
     const url = request.nextUrl.clone();
     url.pathname = '/dashboard';
     return NextResponse.redirect(url);
   }
 
-  if (!authenticated && pathname !== '/login') {
+  if (!isLoggedIn && pathname !== '/login') {
     const url = request.nextUrl.clone();
     url.pathname = '/login';
     return NextResponse.redirect(url);
@@ -21,7 +24,6 @@ export async function middleware(request: any) {
   else {
     return NextResponse.next();
   }
-
 }
 
 export const config = {
