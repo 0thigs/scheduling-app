8a1c1f3221df5d2ac06e702c5de6f14f7c4801bf
0thigs <machinimavlogs@gmail.com>
2024-10-12 23:51:56 -0300

    🖥 scripts: change the auth way

--- src/server/services/userService.ts
+++ src/server/services/userService.ts
@@ -1,21 +1,29 @@
 import supabase from "../../utils/supabaseClient";
 
-export async function addUser(name: string, email: string, password: string) {
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email: email,
-            password: password,
-        })
-        if (error) {
-        console.log(email, password)
-        console.log("Error adding user:", error);
+export async function addUser(name: string, email: string) {
+    const { data, error } = await supabase
+        .from("users")
+        .insert([
+            {
+                name,
+                email,
+            },
+        ])
+        .single();
+    if (error) {
+        console.log("Erro ao adicionar usuário:", error.message);
         return false;
     }
     return true;
 }
 
 export async function getUserData(userId: string) {
-    const {data} = await supabase.auth.getUser()
-    let metadata = data.user?.user_metadata
-    console.log(metadata)
+    const { data, error } = await supabase.auth.getUser(userId);
+    if (error) {
+        console.log("Erro ao obter dados do usuário:", error.message);
+        return null;
+    }
+    let metadata = data.user?.user_metadata;
+    console.log(metadata);
     return metadata;
 }
