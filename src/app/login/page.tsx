b3540b589b4bfb26fe1a2ea161dbdfc1757d70db
0thigs <machinimavlogs@gmail.com>
2024-10-12 23:49:40 -0300

    🧱component: update handlesubmit and authcheck functions

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,58 +1,67 @@
 "use client";
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import Input from '../components/input';
-import { Login_Register_Background } from '../components/login_register_background';
-import { toast } from 'react-toastify';
-import { Button } from '@nextui-org/button';
-import AuthUser from '../../server/auth/authUser';
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Input from "../components/input";
+import { Login_Register_Background } from "../components/login_register_background";
+import { toast } from "react-toastify";
+import { Button } from "@nextui-org/button";
+import AuthUser from "../../server/auth/authUser";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isSent, setIsSent] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSending, setIsSending] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const authUser = new AuthUser();
 
   useEffect(() => {
     async function checkAuth() {
-      const isLoggedIn = await authUser.isAuth();
-      setIsAuthenticated(isLoggedIn);
-      console.log("USUARIO AUTENTICADO NA ROTA DE LOGIN", isLoggedIn)
-      if (isLoggedIn) {
-        window.location.href = '/dashboard';
-        return;
+      try {
+        const isLoggedIn = await authUser.isAuth();
+        setIsAuthenticated(isLoggedIn);
+        console.log("USUÁRIO AUTENTICADO NA ROTA DE LOGIN", isLoggedIn);
+        if (isLoggedIn) {
+          window.location.href = "/dashboard";
+        }
+      } catch (error) {
+        console.error("Erro ao verificar autenticação:", error);
       }
     }
     checkAuth();
-  }, []);
-  
-  const handleSubmit = async (e:any) => {
+  }, [authUser]);
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
+    setIsSending(true);
     try {
       const result = await authUser.handleLogin(email, password);
-      if (result !== null && result !== undefined) {
-        setIsSent("true");
-        toast.success("Link de login enviado para o seu email!");
-      } else {
-        setIsSent("false");
-        toast.error("Falha ao enviar o link de login. Por favor, tente novamente.");
+      if (result.user) {
+        toast.success("Login realizado com sucesso!");
+        window.location.href = "/dashboard";
       }
-    } catch (error) {
-      console.error("Erro ao fazer login:", error);
-      setIsSent("false");
-      toast.error("Ocorreu um erro ao tentar fazer login. Por favor, tente novamente.");
+    } catch (error: any) {
+      console.error("Erro ao fazer login:", error.message);
+      toast.error(`Falha no login: ${error.message}`);
+    } finally {
+      setIsSending(false);
     }
   };
 
   return (
     <div className="flex flex-col h-screen lg:flex-row">
       <div className="flex flex-col items-center justify-center w-full p-8 bg-white lg:w-1/2">
-        <h1 className="mb-8 text-3xl font-bold lg:mb-20 lg:text-4xl">Salon Appointment</h1>
-        <form onSubmit={handleSubmit} className="w-full max-w-md p-5 rounded-md border border-[#D9D9D9]">
+        <h1 className="mb-8 text-3xl font-bold lg:mb-20 lg:text-4xl">
+          Salon Appointment
+        </h1>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md p-5 rounded-md border border-[#D9D9D9]"
+        >
           <h2 className="mb-6 text-xl font-semibold lg:text-2xl">Logar</h2>
           <div className="mb-4">
-            <label className="block mb-2" htmlFor="email">Email</label>
+            <label className="block mb-2" htmlFor="email">
+              Email
+            </label>
             <Input
               type={"email"}
               id={"email"}
@@ -60,11 +69,12 @@ const LoginPage = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               classname={"w-full p-2 border border-gray-300 rounded"}
-            >
-            </Input>
+            />
           </div>
           <div className="mb-4">
-            <label className="block mb-2" htmlFor="email">Senha</label>
+            <label className="block mb-2" htmlFor="password">
+              Senha
+            </label>
             <Input
               type={"password"}
               id={"password"}
@@ -72,33 +82,31 @@ const LoginPage = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               classname={"w-full p-2 border border-gray-300 rounded"}
-            >
-            </Input>
+            />
           </div>
           <div className="flex items-center justify-between mb-4">
             <Link
-              className='transition-all duration-150 text-zinc-500 hover:text-zinc-700'
+              className="transition-all duration-150 text-zinc-500 hover:text-zinc-700"
               href={"/register"}
             >
               Registrar-se
             </Link>
           </div>
-          {
-          isSent === "true" && 
-          <p className="p-4 mb-4 text-green-600 bg-green-100 border border-green-300 rounded-md">
-                Link de login enviado para o seu email!
-          </p>
-          }
-          {
-          isSent === "false" &&
-          <p className="p-4 mb-4 text-red-600 bg-red-100 border border-red-300 rounded-md">
-                Algo deu errado!
-          </p>
-          }
-          <Button type='submit' className='w-full p-2 text-white bg-black rounded'>Login</Button>
+          {isSending && (
+            <p className="p-4 mb-4 text-blue-600 bg-blue-100 border border-blue-300 rounded-md">
+              Processando login...
+            </p>
+          )}
+          <Button
+            type="submit"
+            className="w-full p-2 text-white bg-black rounded"
+            disabled={isSending}
+          >
+            {isSending ? "Conectando..." : "Login"}
+          </Button>
         </form>
       </div>
-      <Login_Register_Background/>
+      <Login_Register_Background />
     </div>
   );
 };
