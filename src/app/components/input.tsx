cb3efe465e843a90bbceab0d448402a390976d82
0thigs <machinimavlogs@gmail.com>
2024-11-25 22:38:18 -0300

    🧱component: update input

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 
 type InputProps = {
-    type: string
-    id: string
-    placeholder: string
-    value: string
-    classname: string
-    onChange: (e: any) => void
-}
+  type: string;
+  id: string;
+  placeholder: string;
+  value: string;
+  classname: string;
+  onChange: (e: any) => void;
+};
 
-const Input = ({type, id, placeholder, value, classname, onChange, }: InputProps) => {
+const Input = ({
+  type,
+  id,
+  placeholder,
+  value,
+  classname,
+  onChange,
+}: InputProps) => {
   return (
     <input
       type={type}
